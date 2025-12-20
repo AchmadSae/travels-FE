7af45ba1a079f45e91e67595732fa91c08e7f9e6
esarizki15 <esarizki15@gmail.com>
2025-12-20 13:26:56 +0700

    customer feedback

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -95,13 +95,57 @@
       </div>
     </div>
     <div class="bg-[#414D55] w-full text-white">
-      <div class="flex justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:py-14">
-        <div class="flex flex-col gap-2">
-          <div class="text-2xl md:text-4xl">
-            Our Customer Feedback
+      <div class="flex gap-14 flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:py-14">
+        <div class="flex justify-between">
+          <div class="flex flex-col gap-2">
+            <div class="text-2xl md:text-4xl">
+              Our Customer Feedback
+            </div>
+            <div class="text-lg md:text-2xl">
+              Don’t take our word for it. Trust our customers
+            </div>
           </div>
-          <div class="text-lg md:text-2xl">
-            Don’t take our word for it. Trust our customers
+          <!-- Navigator in here -->
+        </div>
+        <div class="grid grid-cols-4 gap-4 max-w-5xl justify-center items-center mx-auto">
+          <div
+            v-for="value in [1, 1, 1, 1]"
+            :key="value"
+            class="bg-white rounded-2xl shadow-lg p-6 max-w-sm w-full"
+          >
+            <div class="flex items-center gap-4 mb-4">
+              <NuxtImg
+                src="https://i.redd.it/e3zaq33rfn791.jpg"
+                class="w-16 h-16 rounded-lg object-cover"
+              />
+
+              <div class="flex gap-1 text-amber-400">
+                <svg
+                  v-for="value in [1, 1, 1, 1, 1]"
+                  :key="value"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  class="w-6 h-6"
+                  value
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.007Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <h3 class="text-2xl font-bold text-slate-900 mb-3">
+              Floyd Miles
+            </h3>
+
+            <p class="text-slate-600 text-base leading-relaxed line-clamp-3">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+              sint. Velit officia consequat duis enim v...
+            </p>
           </div>
         </div>
       </div>
