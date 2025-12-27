c6d3f75d675e5a5c22e7fb1ef447dc38a28e9c6d
esarizki15 <esarizki15@gmail.com>
2025-12-27 14:48:11 +0700

    push

--- app/pages/artikel/index.vue
+++ app/pages/artikel/index.vue
@@ -2,17 +2,23 @@
   <div class="bg-gradient-to-br from-white to-gray-400">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="md:min-h-screen" />
-      <div class="md:grid grid-cols-12 mb-8 gap-12">
-        <div class="col-span-8 flex flex-col md:gap-8 gap-12">
-          <ArtikelCard
-            v-for="value in [1, 1, 1, 1]"
-            :key="value"
-          />
-        </div>
-        <div class="col-span-1 max-sm:hidden" />
-        <div class="col-span-3 flex flex-col gap-8">
+      <div class="flex flex-col gap-8">
+        <div class="flex flex-col gap-8 md:hidden">
           <ArtikelCategory />
           <ArtikelTag />
+        </div>
+        <div class="md:grid grid-cols-12 mb-8 gap-12">
+          <div class="col-span-8 flex flex-col md:gap-8 gap-12">
+            <ArtikelCard
+              v-for="value in [1, 1, 1, 1]"
+              :key="value"
+            />
+          </div>
+          <div class="col-span-1 max-sm:hidden" />
+          <div class="col-span-3 flex flex-col gap-8 max-sm:hidden">
+            <ArtikelCategory />
+            <ArtikelTag />
+          </div>
         </div>
       </div>
     </div>
